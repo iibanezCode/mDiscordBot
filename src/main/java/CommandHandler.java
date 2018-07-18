8fca59140182d052872c7d9932447508fc5758b5
iibanezCode <ibanezrubio.igor@gmail.com>
2018-07-18 21:32:43 +0200

    Dev-Update: .test working

---
+++ src/main/java/CommandHandler.java
@@ -0,0 +1,52 @@
+import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+import java.util.*;
+
+public class CommandHandler {
+
+    @EventSubscriber
+    public void onMessageReceived(MessageReceivedEvent event){
+
+        // Note for error handling, you'll probably want to log failed commands with a logger or sout
+        // In most cases it's not advised to annoy the user with a reply incase they didn't intend to trigger a
+        // command anyway, such as a user typing ?notacommand, the bot should not say "notacommand" doesn't exist in
+        // most situations. It's partially good practise and partially developer preference
+
+        // Given a message "/test arg1 arg2", argArray will contain ["/test", "arg1", "arg"]
+        String[] argArray = event.getMessage().getContent().split(" ");
+
+        // First ensure at least the command and prefix is present, the arg length can be handled by your command func
+        if(argArray.length == 0)
+            return;
+
+        // Check if the first arg (the command) starts with the prefix defined in the utils class
+        if(!argArray[0].startsWith(BotUtils.BOT_PREFIX))
+            return;
+
+        // Extract the "command" part of the first arg out by just ditching the first character
+        String commandStr = argArray[0].substring(1);
+
+        // Load the rest of the args in the array into a List for safer access
+        List<String> argsList = new ArrayList<>(Arrays.asList(argArray));
+        argsList.remove(0); // Remove the command
+
+        // Begin the switch to handle the string to command mappings. It's likely wise to pass the whole event or
+        // some part (IChannel) to the command handling it
+        switch (commandStr) {
+
+            case "test":
+                testCommand(event, argsList);
+                break;
+
+        }
+    }
+
+
+    private void testCommand(MessageReceivedEvent event, List<String> args){
+
+        BotUtils.sendMessage(event.getChannel(), "You ran the test command with args: " + args);
+
+    }
+
+}
